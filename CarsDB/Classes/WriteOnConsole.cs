2fe20e95834f81972e73ea50fc5ff6c2323408cc
godanyidavid <godanyi.david63@gmail.com>
2023-06-23 15:15:33 +0200

    Create ToConsole

---
+++ CarsDB/Classes/WriteOnConsole.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarsDB.Classes
+{
+    static class WriteOnConsole
+    {
+        public static void ToConsole<T>(this IEnumerable<T> input, string header)
+        {
+            Console.WriteLine("***" + header + "***");
+            foreach (T item in input)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.WriteLine("***" + header + "***");
+            Console.ReadLine();
+        }
+    }
+}
