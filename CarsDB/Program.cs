11f7770f0ebc5d798d1a041beb8fb31b360f5fe7
godanyidavid <godanyi.david63@gmail.com>
2023-06-25 10:15:59 +0200

    Small changes v1

--- CarsDB/Program.cs
+++ CarsDB/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using CarsDB.Classes;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 //Data Source=(LocalDB)\MSSQLLocalDB; AttachDbFilename=|Data Directory|\Database1.mdf;
 //Integrated Security=True; MultipleACtiveResultSets=true
@@ -8,7 +12,18 @@ namespace CarsDB
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CarDBContext db1 = new CarDBContext();
+            db1.Brand.Select(x => x.Name).ToConsole("Available Brands");
+            db1.Car.Select(x => x.Brand.Name + " " +  x.Type).ToConsole("All");
+
+            var L1 = from car in db1.Car
+                     group car by car.BrandID into grp
+                     select new { Brand = grp.Key, AvgPrice = grp.Average(x => x.PriceInMillion) };
+
+            var L1S = from grp in L1
+                      join brand in db1.Brand on grp.Brand equals brand.Id
+                      select new { Brand = grp.Brand, AvgPrice = grp.AvgPrice };
+            L1S.ToConsole("Avg price for every brand");
         }
     }
 }
