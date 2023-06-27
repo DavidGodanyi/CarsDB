980f6897a5fa5212eb52a078bb9f58a32dd10fc7
godanyidavid <godanyi.david63@gmail.com>
2023-06-27 16:26:36 +0200

    Change the logic

---
+++ CarsDB.Logic/NewestBrandStatistics.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarsDB.Logic
+{
+    public class NewestBrandStatistics
+    {
+        public string newestBrandName { get; set; }
+        public int newestBrandYear { get; set; }        
+
+        public override string ToString()
+        {
+            return "At the very latest founded brand is " + newestBrandName + ", founded in " + newestBrandYear + ".";
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj is NewestBrandStatistics)
+            {
+                NewestBrandStatistics other = obj as NewestBrandStatistics;
+                return this.newestBrandName == other.newestBrandName && this.newestBrandYear == other.newestBrandYear;
+            }
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return newestBrandName.GetHashCode() + newestBrandYear * 2;
+        }
+    }
+}
---
+++ CarsDB.Logic/OldestBrandStatistics.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarsDB.Logic
+{
+    class OldestBrandStatistics
+    {
+        public string oldestBrandName { get; set; }
+        public int oldestBrandYear { get; set; }
+
+        public override string ToString()
+        {
+            return "The earliest founded brand is " + oldestBrandName + ", founded in " + oldestBrandYear + ".";
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj is OldestBrandStatistics)
+            {
+                OldestBrandStatistics other = obj as OldestBrandStatistics;
+                return this.oldestBrandName == other.oldestBrandName && this.oldestBrandYear == other.oldestBrandYear;
+            }
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return oldestBrandName.GetHashCode() + oldestBrandYear * 3;
+        }
+    }
+}
